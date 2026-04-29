6d15fda2b60276e39d7c7b5213e78b42ede5e347
Chandan <sinhachandan4992@gmail.com>
2026-04-29 15:57:33 +0530

    first commit

---
+++ RavanApplication.java
@@ -0,0 +1,13 @@
+package com.ravan.ravan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavanApplication.class, args);
+	}
+
+}
---
+++ RavanController.java
@@ -0,0 +1,15 @@
+package com.ravan.ravan;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RavanController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("message", "Hello Thymeleaf");
+        return "index"; // index.html ko call karega
+    }
+}

aca1d665889e1050fa572c496dad26e9afb9aaea
Chandan <sinhachandan4992@gmail.com>
2026-05-01 16:46:21 +0530

    add full project with index.html

---
+++ src/test/java/com/ravan/ravan/RavanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravan.ravan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
